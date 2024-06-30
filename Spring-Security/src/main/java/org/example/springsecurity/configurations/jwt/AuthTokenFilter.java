70a5c011bd33645b4914ebd03219619d109c2a33
Duy.T.C <duytran.81811@gmail.com>
2024-06-30 14:35:57 +0700

    update: update code and refactor code

--- Spring-Security/src/main/java/org/example/springsecurity/configurations/jwt/AuthTokenFilter.java
+++ Spring-Security/src/main/java/org/example/springsecurity/configurations/jwt/AuthTokenFilter.java
@@ -4,45 +4,51 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import org.example.springsecurity.configurations.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.example.springsecurity.configurations.security.UserInfoServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
-    private final UserDetailsServiceImpl userDetailsService;
+    private final UserInfoServiceImpl userDetailsService;
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthTokenFilter.class);
 
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-        var jwtToken = parseJwtToken(request);
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
+        var jwtToken = jwtUtil.parseJwt(request);
         if (jwtUtil.validateJwtToken(jwtToken)) {
             var username = jwtUtil.extractUsername(jwtToken);
             var userDetails = userDetailsService.loadUserByUsername(username);
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(filterChain, userDetails);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,                  // Principal: thông tin người dùng
+                    //TODO                        // Credentials: mật khẩu của người dùng (nếu dùng JWT thì không cần thiết)
+                    userDetails.getAuthorities()  // Authorities: danh sách quyền hạn
+            );
+
+            // Thiết lập chi tiết xác thực từ yêu cầu HTTP
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            // Thiết lập Authentication vào SecurityContext
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
             try {
                 filterChain.doFilter(request, response);
             } catch (IOException | ServletException exception) {
-                LOGGER.error("ServletException IOException [ EXCEPTION ]: => {}", exception.getMessage());
+                LOGGER.error("[ DO-FILTER-INTERNAL ]: => {}", exception.getMessage());
             }
         }
 
     }
 
-    private String parseJwtToken(HttpServletRequest request) {
-        String authorization = request.getHeader("Authorization");
-        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
-            return authorization.startsWith("Bearer ") ? authorization : "";
-        }
-        return "";
-    }
 }
