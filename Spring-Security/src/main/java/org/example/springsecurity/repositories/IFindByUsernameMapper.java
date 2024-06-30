1948c7b36d719ec35ee8dc35a095cbb60deb6c85
Duy.T.C <duytran.81811@gmail.com>
2024-06-30 14:38:56 +0700

    delete: IFindByUsernameMapper

--- Spring-Security/src/main/java/org/example/springsecurity/repositories/IFindByUsernameMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.springsecurity.repositories;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.example.springsecurity.configurations.security.UserDetailsImpl;
-
-@Mapper
-public interface IFindByUsernameMapper {
-    UserDetailsImpl findByUsername(String name);
-}
