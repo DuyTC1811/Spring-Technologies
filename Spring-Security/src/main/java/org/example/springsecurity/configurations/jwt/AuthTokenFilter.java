23017779a0659b078ac62dba70df3747487ed635
Duy.T.C <duytran.81811@gmail.com>
2024-07-02 22:46:36 +0700

    update: update code AuthTokenFilter

--- Spring-Security/src/main/java/org/example/springsecurity/configurations/jwt/AuthTokenFilter.java
+++ Spring-Security/src/main/java/org/example/springsecurity/configurations/jwt/AuthTokenFilter.java
@@ -10,12 +10,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
@@ -26,29 +29,35 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
         var jwtToken = jwtUtil.parseJwt(request);
-        if (jwtUtil.validateJwtToken(jwtToken)) {
-            var username = jwtUtil.extractUsername(jwtToken);
-            var userDetails = userDetailsService.loadUserByUsername(username);
-
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    userDetails,                  // Principal: thông tin người dùng
-                    //TODO                        // Credentials: mật khẩu của người dùng (nếu dùng JWT thì không cần thiết)
-                    userDetails.getAuthorities()  // Authorities: danh sách quyền hạn
-            );
-
-            // Thiết lập chi tiết xác thực từ yêu cầu HTTP
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            // Thiết lập Authentication vào SecurityContext
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-            try {
+        try {
+            if (jwtToken == null) {
                 filterChain.doFilter(request, response);
-            } catch (IOException | ServletException exception) {
-                LOGGER.error("[ DO-FILTER-INTERNAL ]: => {}", exception.getMessage());
+                return;
             }
-        }
 
+            var username = jwtUtil.extractUsername(jwtToken);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (username != null && authentication == null) {
+                var userDetails = userDetailsService.loadUserByUsername(username);
+
+                if (jwtUtil.isTokenValid(jwtToken, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,                  // Principal: thông tin người dùng
+                            null,                         // Credentials: mật khẩu của người dùng (nếu dùng JWT thì không cần thiết)
+                            userDetails.getAuthorities()  // Authorities: danh sách quyền hạn
+                    );
+                    // Thiết lập chi tiết xác thực từ yêu cầu HTTP
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // Thiết lập Authentication vào SecurityContext
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (IOException | ServletException exception) {
+            LOGGER.error("[ DO-FILTER-INTERNAL ]: => {}", exception.getCause().getCause().getMessage());
+        }
     }
 
 }
