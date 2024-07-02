985b9a6e4fb60efd1a666c39e01acb89683cb630
Duy.T.C <duytran.81811@gmail.com>
2024-07-02 22:45:37 +0700

    update: format response time

--- Spring-Security/src/main/java/org/example/springsecurity/exceptions/ExceptionResponse.java
+++ Spring-Security/src/main/java/org/example/springsecurity/exceptions/ExceptionResponse.java
@@ -1,17 +1,24 @@
 package org.example.springsecurity.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ExceptionResponse {
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "dd-MM-yyyy HH:MM:SS",
+            timezone = "Asia/Ho_Chi_Minh"
+    )
     private LocalDateTime timestamp = LocalDateTime.now();
     private Integer code;
     private String message;
 
-    public ExceptionResponse(int code,String message) {
+    public ExceptionResponse(int code, String message) {
         this.code = code;
         this.message = message;
     }
