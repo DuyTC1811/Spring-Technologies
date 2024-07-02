e19722ef415857c7027d2bc4b10bb3588c14c854
Duy.T.C <duytran.81811@gmail.com>
2024-07-02 22:47:49 +0700

    update: update code SecurityConfig

--- Spring-Security/src/main/java/org/example/springsecurity/configurations/security/SecurityConfig.java
+++ Spring-Security/src/main/java/org/example/springsecurity/configurations/security/SecurityConfig.java
@@ -2,6 +2,9 @@ package org.example.springsecurity.configurations.security;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springsecurity.configurations.jwt.AuthEntryPointJwt;
+import org.example.springsecurity.configurations.jwt.AuthTokenFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,18 +14,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
     private final AuthEntryPointJwt unauthorizedHandler;
+    private final AuthTokenFilter authTokenFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
         try {
-            httpSecurity.csrf(AbstractHttpConfigurer::disable)
+            httpSecurity
+                    .csrf(AbstractHttpConfigurer::disable)                                  // Disable CSRF protection)
+                    .cors(cors -> cors.configurationSource(corsConfigurationSource()))      // Enable CORS with custom configuration
                     .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(unauthorizedHandler))
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
@@ -32,15 +44,27 @@ public class SecurityConfig {
                                     "/v3/api-docs/**",
                                     "/swagger-resources/**",
                                     "/webjars/**").permitAll()
-                            .requestMatchers("api/account/**").permitAll()
+                            .requestMatchers("api/auth/**").permitAll()
                             .anyRequest().authenticated())
-                    .formLogin(withDefaults())
-                    .rememberMe(withDefaults());
+
+                    .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);  // JWT filter
             return httpSecurity.build();
 
         } catch (Exception exception) {
-            throw new RuntimeException("Error configuring SecurityFilterChain {}", exception);
+            LOGGER.error("[ ERROR ] Configuring SecurityFilterChain {}", exception.getLocalizedMessage());
+            return null;
         }
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
