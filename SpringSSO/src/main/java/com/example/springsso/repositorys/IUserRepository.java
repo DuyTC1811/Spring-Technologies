d9adaef3e264e4fe5725f9abc14825b5e2e02fc0
Duy.T.C <duytran.81811@gmail.com>
2024-09-21 18:30:28 +0700

    [REMOVE]: remove project SpringSSO

--- SpringSSO/src/main/java/com/example/springsso/repositorys/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springsso.repositorys;
-
-import com.example.springsso.entitys.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends JpaRepository<UserEntity, Long> {
-}
