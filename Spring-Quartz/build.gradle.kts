2cb76dfd164fa6557671db5c23b7a29ea26acf37
Duy Tran <duytran.81811@gmail.com>
2025-12-28 16:28:35 +0700

    Spring Quatz

---
+++ Spring-Quartz/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+description = "Spring-Quartz"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-quartz")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-quartz-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ Spring-Quartz/src/test/java/org/example/springquartz/SpringQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
