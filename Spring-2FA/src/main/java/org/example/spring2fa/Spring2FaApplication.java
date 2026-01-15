c55e1ae48063416dcbe1b4dde55f92dcd1021ab9
Duy Tran <duytran.81811@gmail.com>
2026-01-15 07:47:18 +0700

    Spring 2FA

---
+++ Spring-2FA/src/main/java/org/example/spring2fa/Spring2FaApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2FaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2FaApplication.class, args);
+    }
+
+}
---
+++ Spring-2FA/src/test/java/org/example/spring2fa/Spring2FaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2FaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
