33da1dbd6cc2fdfcfa658e1dcfad2d83d9882d77
Duy Tran <duytran.81811@gmail.com>
2026-04-27 13:34:45 +0700

    Add permission handling to UserInfoServiceImpl and integrate IPermissionsMapper:
    - Introduce `IPermissionsMapper` interface and corresponding MyBatis XML mapper.
    - Update `UserInfoServiceImpl` to fetch and set user permissions.
    - Add `permissions.xml` to define SQL query for fetching permissions.
    - Remove redundant `ENABLED` check from `roles.xml`.

--- Spring-Security/src/main/java/org/example/springsecurity/configurations/security/UserInfoServiceImpl.java
+++ Spring-Security/src/main/java/org/example/springsecurity/configurations/security/UserInfoServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.springsecurity.configurations.security;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springsecurity.mappers.IAuthenticationMapper;
+import org.example.springsecurity.mappers.IPermissionsMapper;
 import org.example.springsecurity.mappers.IRoleMapper;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,15 +16,19 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class UserInfoServiceImpl implements UserDetailsService {
     private final IAuthenticationMapper authMapper;
+    private final IPermissionsMapper permissionMapper;
     private final IRoleMapper roleMapper;
     @Override
     public UserInfo loadUserByUsername(String username) {
+
         UserInfo userInfo = authMapper.findByUsername(username);
         if (userInfo == null) {
             throw new UsernameNotFoundException(username);
         }
         Set<String> roleCodes = roleMapper.findRoleByUserId(userInfo.getUserId());
+        Set<String> permissions = permissionMapper.findPermissionsByUserId(userInfo.getUserId());
         userInfo.setRoleCodes(roleCodes);
+        userInfo.setPermissionCodes(permissions);
         return userInfo;
     }
 }
